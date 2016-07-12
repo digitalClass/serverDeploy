b04f74a9953f62db376aa6b9f65218edcb31479a
hzh8311 <hzh8311@mail.ustc.edu.cn>
2016-07-12 23:33:58 +0800

    delte execise code

--- digitalClass/digitalClass/views.py
+++ digitalClass/digitalClass/views.py
@@ -19,18 +19,3 @@ def current_datetime(request):
     now = datetime.datetime.now()
     return render_to_response('current_datetime.html',{'current_datetime': now})
 
-def days_ahead(request, offset):
-    """print offset days ahead current_datetime
-
-    :request: TODO
-    :offset: TODO
-    :returns: TODO
-
-    """
-    try:
-        days_offset = int(offset)
-    except ValueError as e:
-        raise Http404
-    future_datetime = datetime.datetime.now() + datetime.timedelta(days=days_offset)
-    return render_to_response('future_time.html',locals())
-
