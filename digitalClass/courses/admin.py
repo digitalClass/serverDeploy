095dc06404bab0ffecbb9b0d05a0f57da9955d19
Yunfeng Wang <wangyf11@mail.ustc.edu.cn>
2016-07-13 11:46:29 +0800

    remove register info in courses/admin.py and add comments app in setting file

--- digitalClass/courses/admin.py
+++ digitalClass/courses/admin.py
@@ -2,9 +2,3 @@ from django.contrib import admin
 
 # Register your models here.
 from courses.models import *
-
-admin.site.register(User2)
-admin.site.register(Question)
-admin.site.register(Course)
-admin.site.register(Answer)
-
--- digitalClass/digitalClass/settings.py
+++ digitalClass/digitalClass/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'courses',
+    'comments',
 ]
 
 AUTH_USER_MODEL = 'users.User'
