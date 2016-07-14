4e29e70e7e39111fd80630e74a117c3308470b0e
Yunfeng Wang <wangyf11@mail.ustc.edu.cn>
2016-07-14 17:00:29 +0800

    update comments model, create comments views

--- digitalClass/comments/models.py
+++ digitalClass/comments/models.py
@@ -1,15 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from courses import models as courses_model
-from users import models as users_model
+from courses import models as courses_models
+from users import models as users_models
 # Create your models here.
 class Question(models.Model):
 
 	""" Definition of question posted in course notes"""
 	date = models.DateField()
-	user = models.ForeignKey(users_model.User)
-	course = models.ForeignKey(courses_model.Course)
+	user = models.ForeignKey(users_models.User, null=True)
+	course = models.ForeignKey(courses_models.Course, null=True)
+	ppt_slice = models.ForeignKey(courses_models.PPTslice, null=True)
 	content = models.CharField(max_length=1024)
 	num_vote = models.IntegerField()
 
@@ -21,8 +22,9 @@ class Answer(models.Model):
 
 	""" Definition of answer posted in course notes"""
 	date = models.DateField()
-	user = models.ForeignKey(users_model.User)
-	course = models.ForeignKey(courses_model.Course)
+	user = models.ForeignKey(users_models.User, null=True)
+	course = models.ForeignKey(courses_models.Course, null=True)
+	questin = models.ForeignKey(Question, null=True)
 	user_type = models.IntegerField()
 	content = models.CharField(max_length=1024)
 	num_vote = models.IntegerField()
@@ -35,8 +37,8 @@ class Question_Comment(models.Model):
 
 	""" Definition of comment on question posted in course notes"""
 	date = models.DateField()
-	question = models.IntegerField()
-	user = models.ForeignKey(users_model.User)
+	question = models.ForeignKey(Question, null=True)
+	user = models.ForeignKey(users_models.User, null=True)
 	content = models.CharField(max_length=1024)
 
 	def __unicode__(self):
@@ -47,8 +49,8 @@ class Answer_Comment(models.Model):
 
 	""" Definition of comment on question posted in course notes"""
 	date = models.DateField()
-	answer = models.IntegerField()
-	user = models.ForeignKey(users_model.User)
+	answer = models.ForeignKey(Answer, null=True)
+	user = models.ForeignKey(users_models.User, null=True)
 	content = models.CharField(max_length=1024)
 
 	def __unicode__(self):
--- digitalClass/comments/views.py
+++ digitalClass/comments/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
+from comments.models import Question, Answer, Question_Comment, Answer_Comment
+from courses.models import PPTslice
 # Create your views here.
+def get_question(course_id, ppt_id):
+	curr_slice = PPTslice.objects.get(index=ppt_id)	
+	questions = Question.objects.filter(course=course_id, ppt_slice=curr_slice)
+
+	#return whole Question models because we need to use them to get
+	#Answers and other elements.
+	return questions
+
+
+def get_answer(q):
+	answers= Answer.objects.filter(question=q)	
+	return answers 
+
+
+def get_question_comment(q):
+	question_comments = Question_Comment.objects.filter(question=q)	
+	return question_comments
+
+
+def get_answer_comment(a):
+	answer_comments = Answer_Comment.objects.filter(answer=a)	
+	return answer_comments
+
+
+
+	
+
+	
