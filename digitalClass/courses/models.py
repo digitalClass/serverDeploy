2ed352563a607f2e1b717a5865cae3947e711bca
Cobbyzhang <616759967@qq.com>
2016-07-14 22:45:45 +0800

    修改：course表新增创建时间和课程名称属性，pptfile,video表增加上传时间和名称属性

--- digitalClass/courses/models.py
+++ digitalClass/courses/models.py
@@ -4,21 +4,32 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Course(models.Model):
-	id = models.IntegerField(primary_key=true)
+	#采用内置自增id
+	#id = models.IntegerField(primary_key=true)
 	date = models.DateField()
+	#课程创建时间
+	create_time = models.DateTimeField()
 	#the image path of this course showcases on courses list.
 	img_path = models.CharField(max_length=256)
+	#课程名称
+	title = models.CharFiedl(max_length=32)
 	course_id = models.CharField(max_length=16)
 	teacher = models.ForeignKey(User)
 	ta = models.ForeigbKey(User)
 
 class PPTfile(models.Model):
-	id = models.IntegerField(primary_key=true)
+	#采用内置自增id
+	#id = models.IntegerField(primary_key=true)
 	date = models.DateField()
+	#PPT上传时间
+	upload_time = models.DateTimeField()
+	#PPT名称
+	title = models.CharFiedl(max_length=32)
 	course = models.ForeignKey(Course)
 
 class PPTslice(models.Model):
-	id = models.IntegerField(primary_key=true)
+	#采用内置自增id
+	#id = models.IntegerField(primary_key=true)
 	#index of a pptfile
 	index = models.IntegerField(max_length=512)
 	date = models.DateField()
@@ -27,10 +38,15 @@ class PPTslice(models.Model):
 	pptfile = models.ForeignKey(PPTfile) 
 	
 class Video(models.Model):
-	id = models.IntegerField(primary_key=true)
+	#采用内置自增id
+	#id = models.IntegerField(primary_key=true)
 	#video index of a course
 	index = models.IntegerField(max_length=64)
 	date = models.DateField()
+	#video上传时间
+	upload_time = models.DateTimeField()
+	#video名称
+	title = models.CharFiedl(max_length=32)
 	video_path = models.CharField(max_length=256)
 	course = models.ForeignKey(Course)
 
