61f5071f63417daf17c2f93fc829a0aa9a217d10
Yunfeng Wang <wangyf11@mail.ustc.edu.cn>
2016-07-16 23:18:53 +0800

    create feedback model in digitalClass directory

---
+++ digitalClass/digitalClass/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from courses import models as courses_models
+from users import models as users_models
+
+class Feedback(models.Model):
+
+	""" Definition of question posted in course notes"""
+	date = models.DateField()
+	user = models.ForeignKey(users_models.User,null=True)
+	content = models.CharField(max_length=1024)
+
+	def __unicode__(self):
+		return self.content
