607b1dedd771c7034bf130e165a76ec500107013
Cobby's Laptop <616759967@qq.com>
2016-07-19 19:27:49 +0800

    add subscription function

--- digitalClass/courses/views.py
+++ digitalClass/courses/views.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from django.shortcuts import render,render_to_response
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect,Http404,HttpResponse
@@ -89,17 +90,33 @@ def course_page(request, c_id):
     except ValueError:
 	raise Http404()
     try:
-        c = Course.objects.get(id=course_id)
+        course = Course.objects.get(id=course_id)
     except Course.DoesNotExist:
 	return HttpResponse('Course does not exist')
-    ppts = c.pptfile_set.all()
+    ppts = course.pptfile_set.all()
     Is_this_course_teacher = False
+    Is_subscribed = False
+    
     if request.user.is_authenticated():
 	user_id = request.user.id
-	u = c.teacher.filter(id=user_id)
+	u = course.teacher.filter(id=user_id)
 	if u:
 	    Is_this_course_teacher=True
-    return render_to_response('course.html',{'course':c, 'ppts':ppts, 'Is_this_course_teacher':Is_this_course_teacher})
+	s = course.subscribed_user.filter(id=user_id)
+	if s:
+	    Is_subscribed = True
+        if request.method == 'POST':
+	    if request.subscribed_status_changed:
+		if Is_subscribed:
+		    course.subscribed_user.delete()
+		    Is_subscribed = False
+		else:
+		    course.subscribed_user.add(request.user)
+		    Is_subscribed = True
+	    return render_to_response('course.html',{'course':course, 'ppts':ppts, 'Is_this_course_teacher':Is_this_course_teacher, 'Is_subscribed':Is_subscribed},context_instance=RequestContext(request))
+	else:
+	    return HttpResponseRedirect('/accounts/login/')
+    return render_to_response('course.html',{'course':course, 'ppts':ppts, 'Is_this_course_teacher':Is_this_course_teacher, 'Is_subscribed':Is_subscribed})
 
 def course_test(request, c_id):
     #course page
