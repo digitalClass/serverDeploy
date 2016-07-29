7538b2f77137f21e6c2b183db10ad43375f70eb7
Yunfeng Wang <wangyf11@mail.ustc.edu.cn>
2016-07-29 10:29:24 +0800

    update url, remove deprecated operations

--- digitalClass/digitalClass/urls.py
+++ digitalClass/digitalClass/urls.py
@@ -57,5 +57,7 @@ if settings.DEBUG:
         url(r'^course_test/(\d+)/$', courses_views.course_test),
         url(r'^course_test/(\d+)/ppt_upload$', courses_views.ppt_upload),
 ]
-    urlpatterns += patterns('',
-	(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}))
+    urlpatterns += [
+	url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+	]
+
