316159c9f0f903d8231a6f1dd860d2b30c30bf60
Cobbyzhang <616759967@qq.com>
2016-09-06 15:57:30 +0800

    修改task.split_pdf_background 函数

--- digitalClass/courses/tasks.py
+++ digitalClass/courses/tasks.py
@@ -4,8 +4,8 @@ from celery.task import task
 from digitalClass.utils import *
 
 @task()
-def split_pdf_background(name):
-    split_pdf(name)
+def split_pdf_background(pdf_path,course_id,ppt_title,create_img=False,save_dir=None):
+    split_pdf(pdf_path,course_id,ppt_title,create_img,save_dir)
     
 
 
